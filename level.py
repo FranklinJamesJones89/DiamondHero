8fa3d77ea640d5e6d3065ea9018b42958ecf5135
Franklin Jones <=>
2022-09-06 15:10:02 -0400

    Added jump mechanic and side scrolling

--- level.py
+++ level.py
@@ -1,10 +1,10 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import *
 from player import Player
 
 class Level(pygame.sprite.Sprite):
-    def __init__(self, level_data, surface):
+    def __init__(self,level_data,surface):
         # Level setup
         self.display_surface = surface
         self.setup_level(level_data)
@@ -26,15 +26,31 @@ class Level(pygame.sprite.Sprite):
                    player_sprite = Player((x,y))
                    self.player.add(player_sprite)
 
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4)  and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
                
     def run(self):
         # Level tilles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
-
+        
         # Player
         self.player.update()
         self.player.draw(self.display_surface)
+        self.scroll_x()
 
     
     
